3e8f2f6e925dceb3e252b2f2195e4d188ffa727d
jlooper <jen.looper@gmail.com>
2017-04-29 18:58:04 -0400

    homepage cards

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AngularFire, FirebaseListObservable} from 'angularfire2';
+import {DomSanitizer} from '@angular/platform-browser';
+
 
 @Component({
   selector: 'home',
   templateUrl: 'home.component.html'
 })
 
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  items: FirebaseListObservable<any[]>;
+
+  cleanedImage: any;
+  private sub:any;
+
+  constructor(private af: AngularFire, private sanitizer: DomSanitizer, private activatedRoute: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(){
+    this.items = this.af.database.list('/Profile', {
+      query: {
+        orderByChild: 'member',
+        equalTo: true
+      }
+      
+    });
+    this.items.subscribe(queriedItems => {
+        for (let prop in queriedItems){
+              this.cleanedImage = this.sanitizer.bypassSecurityTrustUrl(queriedItems[prop].image);
+              queriedItems[prop].image = this.cleanedImage.changingThisBreaksApplicationSecurity;
+        }  
+    });
+  }
+
+
+}
