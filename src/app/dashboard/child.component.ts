335e7a01e5ded70d70a86a378958f4fc4e74d786
jlooper <jen.looper@gmail.com>
2018-11-03 13:27:42 -0400

    allowing edits to id, and fixing signup error

--- src/app/dashboard/child.component.ts
+++ src/app/dashboard/child.component.ts
@@ -6,20 +6,17 @@ import * as firebase from 'firebase';
 import { Http } from '@angular/http';
 import { map } from 'rxjs/operator/map';
 
-
 // profile
 @Component({
   selector: 'home',
-  templateUrl: 'home.component.html'
+  templateUrl: 'home.component.html',
 })
-
 export class HomeComponent implements OnInit {
-
   // private profiles: FirebaseListObservable<any[]>;
   private profiles: AngularFireList<any[]>;
   users: Array<any[]>;
 
-  constructor(private db: AngularFireDatabase) { }
+  constructor(private db: AngularFireDatabase) {}
 
   ngOnInit() {
     //  show only profiles with activities
@@ -28,24 +25,23 @@ export class HomeComponent implements OnInit {
       this.users = [];
       for (const prop in queriedItems) {
         if ((<any>queriedItems[prop]).Activities) {
-          this.users.push(queriedItems[prop])
+          this.users.push(queriedItems[prop]);
         }
       }
     });
   }
   generateArray(obj) {
-    return Object.keys(obj).map((key) => { return obj[key] });
+    return Object.keys(obj).map(key => {
+      return obj[key];
+    });
   }
-
 }
 // my account
 @Component({
   selector: 'account',
-  templateUrl: 'account.component.html'
+  templateUrl: 'account.component.html',
 })
-
 export class AccountComponent {
-
   user: Observable<firebase.User>;
 
   success: string;
@@ -62,20 +58,20 @@ export class AccountComponent {
   checkins: AngularFireList<Observable<any[]>>;
 
   edit = {
-      name: false,
-      company: false,
-      title: false,
-      bio: false,
-      expertise: false,
-      email: false,
-      address1: false,
-      address2: false,
-      city: false,
-      state: false,
-      postalcode: false,
-      country: false,
-      phone: false,
-      save: false
+    name: false,
+    company: false,
+    title: false,
+    bio: false,
+    expertise: false,
+    email: false,
+    address1: false,
+    address2: false,
+    city: false,
+    state: false,
+    postalcode: false,
+    country: false,
+    phone: false,
+    save: false,
   };
 
   name: string;
@@ -108,91 +104,83 @@ export class AccountComponent {
         this.requests = this.db.list('/Profile/' + this.id + '/Requests');
         this.checkins = this.db.list('/Profile/' + this.id + '/Checkins');
         this.currUser = this.db.list('/Profile/' + this.id + '/User');
-        this.currUser.snapshotChanges().subscribe(
-            data => {
-                this.key = data[0].key;
-            }
-        )
+        this.currUser.snapshotChanges().subscribe(data => {
+          this.key = data[0].key;
+        });
         this.currUser.valueChanges().subscribe(queriedItems => {
-            let i = true;
-            console.log(queriedItems);
-          Object.keys(queriedItems).map(
-            (prop) => {
-                if (i) {
-                    i = false;
-                    console.log('props', queriedItems[prop]);
-                    this.fname = queriedItems[prop].fname;
-                    this.lname = queriedItems[prop].lname;
-                    this.name = queriedItems[prop].fname + ' ' + queriedItems[prop].lname;
-                    this.company = queriedItems[prop].company;
-                    this.title = queriedItems[prop].title;
-                    this.address1 = queriedItems[prop].address1;
-                    this.address2 = queriedItems[prop].address2;
-                    this.city = queriedItems[prop].city;
-                    this.state = queriedItems[prop].state;
-                    this.country = queriedItems[prop].country;
-                    this.postalcode = queriedItems[prop].postalcode;
-                    this.phone = queriedItems[prop].phone;
-                    this.bio = queriedItems[prop].bio;
-                    this.expertise = queriedItems[prop].expertise;
-                }
+          let i = true;
+          console.log(queriedItems);
+          Object.keys(queriedItems).map(prop => {
+            if (i) {
+              i = false;
+              console.log('props', queriedItems[prop]);
+              this.fname = queriedItems[prop].fname;
+              this.lname = queriedItems[prop].lname;
+              this.name = queriedItems[prop].fname + ' ' + queriedItems[prop].lname;
+              this.company = queriedItems[prop].company;
+              this.title = queriedItems[prop].title;
+              this.address1 = queriedItems[prop].address1;
+              this.address2 = queriedItems[prop].address2;
+              this.city = queriedItems[prop].city;
+              this.state = queriedItems[prop].state;
+              this.country = queriedItems[prop].country;
+              this.postalcode = queriedItems[prop].postalcode;
+              this.phone = queriedItems[prop].phone;
+              this.bio = queriedItems[prop].bio;
+              this.expertise = queriedItems[prop].expertise;
             }
-          );
+          });
         });
       }
     });
   }
 
   updateProfile() {
-      const data = {
-          fname: this.fname,
-          lname: this.lname,
-          company: this.company,
-          title: this.title,
-          address1: this.address1,
-          address2: this.address2 || '',
-          city: this.city,
-          state: this.state,
-          country: this.country,
-          postalcode: this.postalcode,
-          phone: this.phone,
-          bio: this.bio,
-          expertise: this.expertise
+    const data = {
+      fname: this.fname,
+      lname: this.lname,
+      company: this.company,
+      title: this.title,
+      address1: this.address1,
+      address2: this.address2 || '',
+      city: this.city,
+      state: this.state,
+      country: this.country,
+      postalcode: this.postalcode,
+      phone: this.phone,
+      bio: this.bio,
+      expertise: this.expertise,
+    };
+    //console.log(data);
+    //console.log('/Profile/' + this.id + '/User');
+    const db = this.db.object('/Profile/' + this.id + '/User/' + this.key);
+    db.update(data).then(success => {
+      this.success = 'Updated!';
+      this.edit = {
+        name: false,
+        company: false,
+        title: false,
+        bio: false,
+        expertise: false,
+        email: false,
+        address1: false,
+        address2: false,
+        city: false,
+        state: false,
+        postalcode: false,
+        country: false,
+        phone: false,
+        save: false,
       };
-      console.log(data);
-      console.log('/Profile/' + this.id + '/User');
-      const db = this.db.object('/Profile/' + this.id + '/User/' + this.key)
-      db.update(data)
-      .then(
-        (success) => {
-              this.edit = {
-                name: false,
-                company: false,
-                title: false,
-                bio: false,
-                expertise: false,
-                email: false,
-                address1: false,
-                address2: false,
-                city: false,
-                state: false,
-                postalcode: false,
-                country: false,
-                phone: false,
-                save: false
-            };
-          this.success = 'Updated!';
-        });
+    });
   }
 }
 // activities
 @Component({
   selector: 'activities',
-  templateUrl: 'activities.component.html'
+  templateUrl: 'activities.component.html',
 })
-
 export class ActivitiesComponent {
-
   user: Observable<firebase.User>;
   activities: AngularFireList<any>;
   id: any;
@@ -207,22 +195,17 @@ export class ActivitiesComponent {
       if (user != null) {
         this.id = user.uid;
         this.email = user.email;
-        this.activities = this.db.list('/Profile/' + this.id + '/Activities')
-
+        this.activities = this.db.list('/Profile/' + this.id + '/Activities');
       }
-    })
-
+    });
   }
 
-
   onSubmit(formData) {
-    const data = this.db.list('/Profile/' + this.id + '/Activities')
-    data.push({ activity: formData.value.activity, date: firebase.database.ServerValue.TIMESTAMP })
-      .then(
-        (success) => {
-          this.success = 'Activity logged!';
-          this.activity = '';
-        });
+    const data = this.db.list('/Profile/' + this.id + '/Activities');
+    data.push({ activity: formData.value.activity, date: firebase.database.ServerValue.TIMESTAMP }).then(success => {
+      this.success = 'Activity logged!';
+      this.activity = '';
+    });
     // .catch(
     //  (err) => {
     //    this.error = err.message;
@@ -233,11 +216,9 @@ export class ActivitiesComponent {
 // request
 @Component({
   selector: 'request',
-  templateUrl: 'request.component.html'
+  templateUrl: 'request.component.html',
 })
-
 export class RequestComponent {
-
   user: Observable<firebase.User>;
   id: any;
   email: string;
@@ -251,13 +232,10 @@ export class RequestComponent {
     this.user.subscribe((user: firebase.User) => {
       if (user != null) {
         this.id = user.uid;
-        this.email = user.email
+        this.email = user.email;
       }
-
-    })
-
+    });
   }
-
 
   handleChange(status, value) {
     if (status) {
@@ -267,14 +245,18 @@ export class RequestComponent {
 
   onSubmit(formData) {
     // push to the user's id within the Requests object
-    const data = this.db.list('/Profile/' + this.id + '/Requests')
-    data.push({ request: formData.value.request, requesttype: this.requesttype, date: firebase.database.ServerValue.TIMESTAMP })
-      .then(
-        (success) => {
-          this.success = 'Request logged!';
-          // empty the value so do not let several shipments of the same
-          this.request = '';
-        });
+    const data = this.db.list('/Profile/' + this.id + '/Requests');
+    data
+      .push({
+        request: formData.value.request,
+        requesttype: this.requesttype,
+        date: firebase.database.ServerValue.TIMESTAMP,
+      })
+      .then(success => {
+        this.success = 'Request logged!';
+        // empty the value so do not let several shipments of the same
+        this.request = '';
+      });
     // .catch(
     // (err) => {
     //    this.error = err.message;
@@ -285,11 +267,9 @@ export class RequestComponent {
 /*checkin*/
 @Component({
   selector: 'checkins',
-  templateUrl: 'checkins.component.html'
+  templateUrl: 'checkins.component.html',
 })
-
 export class CheckinsComponent {
-
   user: Observable<firebase.User>;
   id: any;
   success: string;
@@ -316,26 +296,24 @@ export class CheckinsComponent {
   onSubmit(formData) {
     // push to the user's id within the Requests object
     var date = new Date();
-    var datestring = date.getMonth()+1 + "/" + date.getFullYear();
+    var datestring = date.getMonth() + 1 + '/' + date.getFullYear();
 
-
-    const data = this.db.list('/Profile/' + this.id + '/Checkins')
-    data.push({
-      activities: formData.value.activities,
-      date: datestring,
-      continue: formData.value.continue,
-      feedback: formData.value.feedback
-    })
-      .then(
-        (success) => {
-          this.success = 'Checkin logged! Thank you!';
-          this.activities = '';
-          this.feedback = '';
-        });
+    const data = this.db.list('/Profile/' + this.id + '/Checkins');
+    data
+      .push({
+        activities: formData.value.activities,
+        date: datestring,
+        continue: formData.value.continue,
+        feedback: formData.value.feedback,
+      })
+      .then(success => {
+        this.success = 'Checkin logged! Thank you!';
+        this.activities = '';
+        this.feedback = '';
+      });
     // .catch(
     //  (err) => {
     //    this.error = err.message;
     // })
   }
 }
-
