deca46236bb86c0302e559e82a9b1a9313c35718
Alyssa Nicoll <alyssaamnell@gmail.com>
2019-02-08 16:29:03 -0600

    fix member home dashoard on mobile

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'dashboard',
-  templateUrl: 'dashboard.component.html'
+  templateUrl: 'dashboard.component.html',
+  styleUrls: ['dashboard.styles.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 
 export class DashboardComponent { }
